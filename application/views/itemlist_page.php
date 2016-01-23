044bf3145d5caad82db9fb444367ed71cd6aa3da
NingSama1vs100 <55130500008@st.sit.kmutt.ac.th>
2016-01-23 21:06:07 +0700

    modify header :cat:

--- application/views/itemlist_page.php
+++ application/views/itemlist_page.php
@@ -1,4 +1,17 @@
-<?php $this->load->view('head', array('title' => 'Item List'));?>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Item List</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<?=script_tag('assets/jquery/jquery-2.2.0.min.js')?>
+<?=script_tag('assets/bootstrap/js/bootstrap.js')?>
+<?=link_tag('assets/bootstrap/css/bootstrap.min.css')?>
+<?=link_tag('assets/questio/questio.css')?>
+</head>
 <body>
 <div class="container-fluid">
 	<?php $this->load->view('header', array('title' => 'Item List'));?>
--- application/views/statistics_page.php
+++ application/views/statistics_page.php
@@ -1,7 +1,20 @@
-<?php $this->load->view('head', array('title' => 'Statistics'));?>
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Statistics</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<?=script_tag('assets/jquery/jquery-2.2.0.min.js')?>
+<?=script_tag('assets/bootstrap/js/bootstrap.js')?>
+<?=link_tag('assets/bootstrap/css/bootstrap.min.css')?>
+<?=link_tag('assets/questio/questio.css')?>
+</head>
 <body>
     <div class="container-fluid">
-    <?php $this->load->view('header', array('title' => 'Welcome'));?>
+    <?php $this->load->view('header', array('title' => 'Statistics'));?>
 
     <?php if(!empty($keeperplace)):?>
     <select
