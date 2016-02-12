0b931259d8d0914b773f4f6c24c793cd9d1edc6a
NingSama1vs100 <55130500008@st.sit.kmutt.ac.th>
2016-02-12 22:49:32 +0700

    change get to post :cat:

--- api/select_zone_by_zoneid.php
+++ api/select_zone_by_zoneid.php
@@ -4,7 +4,7 @@ $con=mysql_connect('localhost','root','adminpwd')or die(mysql_error());
 mysql_select_db('questio')or die(mysql_error());
 mysql_query("SET NAMES UTF8");
 $sql="SELECT Zone.*, ZoneType.imageurl AS zonetypeimage FROM Zone
-JOIN ZoneType USING (zonetypeid) WHERE zoneid =".$_POST["zoneid"];
+JOIN ZoneType USING (zonetypeid) WHERE zoneid =".$_GET["zoneid"];
 	$res=mysql_query($sql);
 	while($row=mysql_fetch_assoc($res)){
 	   $output[]=$row;
--- api/select_zoneid_by_qrcode.php
+++ api/select_zoneid_by_qrcode.php
@@ -3,7 +3,7 @@ header("content-type:text/javascript;charset=utf-8");
 $con=mysql_connect('localhost','root','adminpwd')or die(mysql_error()); 
 mysql_select_db('questio')or die(mysql_error());
 mysql_query("SET NAMES UTF8");
-$sql="SELECT zoneid FROM Zone WHERE qrcode =".$_POST["qrcode"];
+$sql="SELECT zoneid FROM Zone WHERE qrcode =".$_GET["qrcode"];
 	$res=mysql_query($sql);
 	while($row=mysql_fetch_assoc($res)){
 	   $output[]=$row;
