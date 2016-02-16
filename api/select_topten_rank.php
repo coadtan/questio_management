8ba67a63659634dba1db44b5b356e69510bcac1d
NingSama1vs100 <55130500008@st.sit.kmutt.ac.th>
2016-02-16 22:21:34 +0700

    add topten rank api :cat:

---
+++ api/select_topten_rank.php
@@ -0,0 +1,23 @@
+<?php
+header("content-type:text/javascript;charset=utf-8");
+$con=mysql_connect('localhost','root','adminpwd')or die(mysql_error()); 
+mysql_select_db('questio')or die(mysql_error());
+mysql_query("SET NAMES UTF8");
+$sql="SELECT rank, adventurerid, displayname, score FROM
+(SELECT func_count_row() AS rank, adventurerid, displayname, score FROM
+(SELECT adventurerid, displayname, SUM(a.score) AS score
+FROM QuestProgress a
+JOIN Adventurer c USING (adventurerid)
+JOIN AdventurerDetails d USING (detailid)
+GROUP BY adventurerid ORDER BY score DESC) b
+ORDER BY rank) c
+WHERE rank BETWEEN 1 and 10";
+if($_REQUEST["key"]=="asdlaekqwekasdlkxzc"){
+	$res=mysql_query($sql);
+	while($row=mysql_fetch_assoc($res)){
+	   $output[]=$row;
+	}
+	print(json_encode($output));
+}
+mysql_close();
+?>
