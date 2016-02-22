91fe147d14e54a8832a03e29d31e169d364304d0
NingSama1vs100 <55130500008@st.sit.kmutt.ac.th>
2016-02-22 09:15:22 +0700

    modified select topten rank :cat:

--- api/select_topten_rank.php
+++ api/select_topten_rank.php
@@ -3,9 +3,9 @@ header("content-type:text/javascript;charset=utf-8");
 $con=mysql_connect('localhost','root','adminpwd')or die(mysql_error()); 
 mysql_select_db('questio')or die(mysql_error());
 mysql_query("SET NAMES UTF8");
-$sql="SELECT rank, adventurerid, displayname, score FROM
-(SELECT func_count_row() AS rank, adventurerid, displayname, score FROM
-(SELECT adventurerid, displayname, SUM(a.score) AS score
+$sql="SELECT rank, guserid, displayname, score FROM
+(SELECT func_count_row() AS rank, guserid, displayname, score FROM
+(SELECT guserid, displayname, SUM(a.score) AS score
 FROM QuestProgress a
 JOIN Adventurer c USING (adventurerid)
 JOIN AdventurerDetails d USING (detailid)
